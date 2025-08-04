b34498e2c098f3ee57106ea1674821e6a7a85af1
胡思宇 <62612263+gottaegbert@users.noreply.github.com>
2025-08-04 10:22:38 +0800

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import dynamic from 'next/dynamic';
 
 export default function Page() {
   return (
-      <GridScene />
+      <h1>
+        hellow world
+      </h1>
   );
 }
--- packages/three-components/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as GridScene } from './GridScene';
-export { default as ThreeCanvas } from './ThreeCanvas';
